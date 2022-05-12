83eb7c1455eb8c7381207ae5c05f7ab0f815289e
anselvar <103392537+anselvar@users.noreply.github.com>
2022-05-11 20:31:38 -0700

    fail test

--- SkillDemo1.java
+++ SkillDemo1.java
@@ -1,3 +1,5 @@
 public class SkillDemo1{
-
+    public static int multiply(int i, int j){
+        return i*j;
+    }
 }
---
+++ SkillDemo1Tester.java
@@ -0,0 +1,9 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class SkillDemo1Tester {
+    @Test
+    public void multiplication(){
+        assertEquals(3, SkillDemo1.multiply(1,2));
+    }
+}
